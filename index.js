25b423e022e7ee924162983d68d6f34c4a0c0d50
Mahdi <mahditajikkazemii@gmail.com>
2024-11-04 01:34:36 +0330

    build the movies route

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
@@ -7,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
