3aba7b10711d171bea5425a85e79192d5e551c08
Mahdi <mahditajikkazemii@gmail.com>
2024-11-05 20:10:32 +0330

    build the rental route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Joi.objectId = require('joi-objectid')(Joi);
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
