f06c1dd02d3961e7a2f479fcbfc580584a0f0cec
Mahdi <mahditajikkazemii@gmail.com>
2024-11-10 19:36:18 +0330

    refactor the users route using lodash

--- models/user.js
+++ models/user.js
@@ -4,19 +4,21 @@ const Joi = require('joi');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        minLength: 3,
+        minLength: 5,
         maxLength: 50,
         required: true
     },
     email: {
         type: String,
         unique: true,
+        minLength: 5,
+        maxLength: 255,
         required: true,
     },
     password: {
         type: String,
         minLength: 8,
-        maxLength: 50,
+        maxLength: 1024,
         required: true
     }
 });
@@ -25,9 +27,9 @@ const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
     const schema = Joi.object({
-        name: Joi.string().min(3).max(50).required(),
-        email: Joi.string().required(),
-        password: Joi.string().min(8).max(50).required()
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().required().email(),
+        password: Joi.string().min(8).max(255).required()
     });
 
     return schema.validate(user);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
@@ -20,15 +21,14 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User already registered.');
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
 
     await user.save();
-
-    res.send(user);
+    
+    res.send(_.pick(user, ['name', 'email', '_id']));
 });
 
 router.put('/:id', async (req, res) => {
