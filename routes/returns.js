ba0944bf777ccdfd9314bf91839d117ce39d6a9d
Mehdi <mahditajikkazemii@gmail.com>
2024-11-18 13:02:57 +0330

    returns command - test second execution path

--- routes/returns.js
+++ routes/returns.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    if(!req.body.customerId) return res.status(400).send('CustomerId was not provided.');
+    
     return res.sendStatus(401);
 });
 
--- tests/integration/routes/returns.test.js
+++ tests/integration/routes/returns.test.js
@@ -1,17 +1,60 @@
 const request = require('supertest');
+const { Rental } = require('../../../models/rental');
+const { User } = require('../../../models/user');
+const mongoose = require('mongoose');
+
 
 describe('/api/returns', () => {
 
     let server;
+    let rental;
+    let customerId;
+    let movieId;
 
-    beforeEach(() => { server = require('../../../index'); });
-    afterEach(async () => { await server.close(); });
+    beforeEach(async () => { 
+        server = require('../../../index');
+
+        customerId = new mongoose.Types.ObjectId();
+        movieId = new mongoose.Types.ObjectId();
+
+        rental = new Rental({
+            customer: {
+                _id: customerId,
+                name: "12345",
+                phone: "12345"
+            },
+            movie: {
+                _id: movieId,
+                title: "test title",
+                dailyRentalRate: 2
+            }
+        });
+        await rental.save();
+    });
+    afterEach(async () => { 
+        await server.close();
+        await Rental.deleteMany({});
+    });
 
     describe('POST /', () => {
         it('Should return 401 if client is not logged in', async () => {
-            const res = await request(server).post('/api/returns');
+            const res = await request(server)
+            .post('/api/returns')
+            .send({ customerId, movieId });
 
             expect(res.status).toBe(401);
+        });
+
+        it('Should return 400 if customerId is not provided', async () => {
+            const token = new User().generateAuthToken();
+
+            const res = await request(server)
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({ movieId });
+                
+
+            expect(res.status).toBe(400);
         });
     });
 });
