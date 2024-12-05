e48e37cd4e82b18b0f299ee609f6a04c6b183575
Mahdi <mahditajikkazemii@gmail.com>
2024-12-05 19:40:03 +0330

    Configure docker properly

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const logger = require('./startup/logger');
-require('express-async-errors');
-const express = require('express');
+const logger = require("./startup/logger");
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3333;
-const server = app.listen(port, () => logger.log('info', `Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  logger.log("info", `Listening on port ${port}...`)
+);
 
 module.exports = server;
